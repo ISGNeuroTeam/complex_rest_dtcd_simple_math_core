373f1b20b1b347197a43481bc3f834affa31b27c
Andrey <astarchenkov@isgneuro.com>
2022-11-22 14:14:08 +0300

    Fixed naming of vars in read.py

--- DataCAD_simple_math_core/translator/read.py
+++ DataCAD_simple_math_core/translator/read.py
@@ -1,14 +1,14 @@
 class Read:
-    WST_BASE_PATH = "SWT"
+    SWT_BASE_PATH = "SWT"
     FORMAT = "CSV"
 
     @classmethod
-    def read(cls, wst_name):
-        return f'readFile format={cls.FORMAT} path={cls.WST_BASE_PATH}/{wst_name}'
+    def read(cls, swt_name):
+        return f'readFile format={cls.FORMAT} path={cls.SWT_BASE_PATH}/{swt_name}'
 
     @classmethod
-    def read_last_row(cls, wst_name):
-        base_read_line = cls.read(wst_name)
+    def read_last_row(cls, swt_name):
+        base_read_line = cls.read(swt_name)
         limit_line = "tail 1"
         complete_line = " | ".join((base_read_line, limit_line))
         return complete_line
