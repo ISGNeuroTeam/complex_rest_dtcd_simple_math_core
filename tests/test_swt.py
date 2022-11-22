f862921ce7a0409375a0e278b1bee317fa590257
Andrey <astarchenkov@isgneuro.com>
2022-11-22 14:38:45 +0300

    Fixed test_swt.py in DRY.

--- tests/test_swt.py
+++ tests/test_swt.py
@@ -6,18 +6,19 @@ from swt import SourceWideTable
 
 class TestSourceWideTable(unittest.TestCase):
 
+    def setUp(self) -> None:
+        self.swt = SourceWideTable("example_of_swt.csv")
+
     def test_read(self):
         with open("resources/example_of_swt.json") as fr:
             first = json.loads(fr.read())
-        swt = SourceWideTable("example_of_swt.csv")
-        second = swt.read()
+        second = self.swt.read()
         self.assertEqual(first, second)
 
     def test_read_last_row(self):
         with open("resources/example_of_swt.json") as fr:
             first = [json.loads(fr.read())[-1]]
-        swt = SourceWideTable("example_of_swt.csv")
-        second = swt.read_last_row()
+        second = self.swt.read_last_row()
         self.assertEqual(first, second)
 
 
