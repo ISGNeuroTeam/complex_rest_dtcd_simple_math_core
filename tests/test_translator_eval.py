c9ff394d4b0cd47239b657a6c9f62e29ce40778b
Andrey <astarchenkov@isgneuro.com>
2022-12-01 11:17:57 +0300

    Updated tests with new compatible eval expressions

--- tests/test_translator_eval.py
+++ tests/test_translator_eval.py
@@ -8,11 +8,11 @@ class TestTranslatorEval(unittest.TestCase):
 
     def test_from_graph(self):
         with open("resources/example_of_graph.json") as fr:
-            first = "eval 'ControlledRichLabelNode01_207.I' = " \
-                    "'StepRichLabelNode11_1.Enabled'*'ControlledRichLabelNode01_207.V'/'TargetRichLabelNode1_1.R' | " \
-                    "eval 'TargetRichLabelNode1_1.P' = " \
-                    "'ControlledRichLabelNode01_207.V'*'ControlledRichLabelNode01_207.I'*'StepRichLabelNode11_1" \
-                    ".Enabled'"
+            first = "eval 'ControlledRichLabelNode01_207.testField' = 5+5.1 | eval 'ControlledRichLabelNode01_207.I'" \
+                    " = 'StepRichLabelNode11_1.Enabled'*'ControlledRichLabelNode01_207.V'/'TargetRichLabelNode1_1.R'" \
+                    " | eval 'TargetRichLabelNode1_1.testField' = cos('TargetRichLabelNode1_1.R') |" \
+                    " eval 'TargetRichLabelNode1_1.P' = 'ControlledRichLabelNode01_207.V'" \
+                    "*'ControlledRichLabelNode01_207.I'*'StepRichLabelNode11_1.Enabled'"
             second = Eval.from_graph(json.loads(fr.read()))
             self.assertEqual(first, second)
 
