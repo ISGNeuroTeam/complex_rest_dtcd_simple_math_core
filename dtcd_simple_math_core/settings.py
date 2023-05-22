3a1704646e541cbaa392a20a43ade94475c105a5
Nikita Serditov <nserditov@isgneuro.com>
2023-05-22 04:48:17 +0300

    added proper logging and connector instance and configs for swt and graph objects

--- dtcd_simple_math_core/settings.py
+++ dtcd_simple_math_core/settings.py
@@ -1,21 +1,102 @@
 import configparser
+import logging.config
 import os
 
 from pathlib import Path
 from core.settings.ini_config import merge_ini_config_with_defaults, configparser_to_dict
+from dtcd_simple_math_core.translator.connector import OTSimpleConnector
+
+__author__ = "Andrey Starchenkov"
+__copyright__ = "Copyright 2023, ISG Neuro"
+__credits__ = ["Nikita Serditov"]
+__license__ = "OT.PLATFORM. License agreement."
+__version__ = "0.2.0"
+__maintainer__ = "Andrey Starchenkov"
+__email__ = "astarchnenkov@isgneuro.com"
+__status__ = "Pre-release"
 
 default_ini_config = {
     'logging': {
-        'level': 'INFO'
+        'level': 'DEBUG'
     },
     'db_conf': {
         'host': 'localhost',
         'port': '5432',
-        'database':  'dtcd_simple_math_core',
+        'database': 'dtcd_simple_math_core',
         'user': 'dtcd_simple_math_core',
         'password': 'dtcd_simple_math_core'
     }
 }
+
+
+def set_logger(loglevel, logfile, logger_name):
+    levels = {
+        'CRITICAL': logging.CRITICAL,
+        'ERROR': logging.ERROR,
+        'WARNING': logging.WARNING,
+        'INFO': logging.INFO,
+        'DEBUG': logging.DEBUG
+    }
+
+    log_dict = {
+        'version': 1,
+        'disable_existing_loggers': False,
+        'formatters': {
+            'standard': {
+                'format': "%(asctime)s %(levelname)-s PID=%(process)d %(module)s:%(lineno)d "
+                          "func=%(funcName)s - %(message)s"
+            },
+            'with_hid': {
+                'format': "%(asctime)s %(levelname)-s PID=%(process)d HID=%(hid)s %(module)s:%(lineno)d "
+                          "func=%(funcName)s - %(message)s"
+            }
+        },
+        'handlers': {
+            'file_handler_standard': {
+                'filename': logfile,
+                'level': levels[loglevel],
+                'class': 'logging.FileHandler',
+                'formatter': 'standard'
+            },
+            'file_handler_with_hid': {
+                'filename': logfile,
+                'level': levels[loglevel],
+                'class': 'logging.FileHandler',
+                'formatter': 'with_hid'
+            },
+            'stream_handler_standard': {
+                'stream': 'ext://sys.stdout',
+                'level': levels[loglevel],
+                'class': 'logging.StreamHandler',
+                'formatter': 'standard'
+            },
+            'stream_handler_with_hid': {
+                'stream': 'ext://sys.stdout',
+                'level': levels[loglevel],
+                'class': 'logging.StreamHandler',
+                'formatter': 'with_hid'
+            },
+        },
+        'loggers': {
+            'osr': {
+                'handlers': ['file_handler_standard', 'stream_handler_standard'],
+                'level': levels[loglevel],
+                'propagate': False
+            },
+            'osr_hid': {
+                'handlers': ['file_handler_with_hid', 'stream_handler_with_hid'],
+                'level': levels[loglevel]
+            },
+        },
+        'root': {
+            'handlers': ['file_handler_standard', 'stream_handler_standard'],
+            'level': levels[loglevel]
+        }
+    }
+
+    logging.config.dictConfig(log_dict)
+    result = logging.getLogger(logger_name)
+    return result
 
 
 # try to read path to config from environment
@@ -27,13 +108,29 @@ else:
     conf_path = Path(conf_path_env).resolve()
 
 config = configparser.ConfigParser()
-
 config.read(conf_path)
-
 config = configparser_to_dict(config)
-
 ini_config = merge_ini_config_with_defaults(config, default_ini_config)
+plugin_name = 'dtcd_simple_math_core'
+CONNECTOR_CONFIG = ini_config['ot_simple_connector']
+EVAL_GLOBALS = ini_config['eval_globals']
+GRAPH_GLOBALS = ini_config['graph_globals']
 
+# db conf
+db_conf = dict(ini_config['db_conf'])
+
+# create connector
+connector = OTSimpleConnector(CONNECTOR_CONFIG)
+
+# set logger
+base_logs_dir = ini_config['general'].get('logs_path', '.')
+logger = set_logger(ini_config['logging'].get('level', 'INFO'),
+                    os.path.join(base_logs_dir, 'dtcd_simple_math_core.log'), plugin_name)
+logger.info('Version: %s' % __version__)
+logger.info('DB configuration: %s' % db_conf)
+
+# set graph key names
+GRAPH_KEY_NAMES = ini_config['graph_key_names']
 
 # configure your own database if you need
 # DATABASE = {
@@ -44,4 +141,3 @@ ini_config = merge_ini_config_with_defaults(config, default_ini_config)
 #         "HOST": ini_config['db_conf']['host'],
 #         "PORT": ini_config['db_conf']['port']
 # }
-
