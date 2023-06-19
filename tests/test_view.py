f04c3a7ebac31596b44fca98b13e7188dbca6114
Nikita Serditov <nserditov@isgneuro.com>
2023-06-19 12:46:51 +0300

    fixed typo

--- tests/test_view.py
+++ tests/test_view.py
@@ -10,5 +10,5 @@ class TestView(TestCase):
         parent_folder = os.path.dirname(__file__)
         with open(f'{parent_folder}/resources/graphs/example_2.json', 'r') as f:
             graph_json_example_body = json.loads(f.read())
-        response = client.post('dtcd_simple_math_core/v1/graph/', graph_json_example_body, format='json')
+        response = client.post('/dtcd_simple_math_core/v1/graph/', graph_json_example_body, format='json')
         self.assertEqual(response.status_code, HTTPStatus.OK)
