16327dabb6582841cb527ca97f8e3d992fadcf03
Nikita Serditov <nserditov@isgneuro.com>
2023-08-22 11:50:39 +0300

    added version to show in config resource

--- dtcd_simple_math_core/views/config_view.py
+++ dtcd_simple_math_core/views/config_view.py
@@ -2,6 +2,8 @@
 """Module to return configuration of the plugin view"""
 # pylint: disable=import-error, too-few-public-methods
 import logging
+import re
+from typing import Union
 
 from rest.views import APIView
 from rest.permissions import AllowAny
@@ -20,7 +22,7 @@ class ConfigView(APIView):
     http_method_names = ['get']
     permission_classes = (AllowAny,)
 
-    def get(self):
+    def get(self, request):
         """Receives nothing
 
         Returns: configuration of the plugin based on a settings.py"""
@@ -32,7 +34,7 @@ class ConfigView(APIView):
                 'handlers': str(self.log.handlers)
             },
             'settings_file_path': str(SETTINGS_FILE_PATH),
-            'plugin_version': PLUGIN_VERSION
+            'plugin_version': PLUGIN_VERSION.strip('"')
         }
 
         return SuccessResponse(result)
